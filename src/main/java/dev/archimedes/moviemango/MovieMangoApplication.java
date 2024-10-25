6fa520792da6e570879ce3232e619b122a7e939f
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-25 22:19:41 +0530

    Initial Commit

---
+++ src/main/java/dev/archimedes/moviemango/MovieMangoApplication.java
@@ -0,0 +1,13 @@
+package dev.archimedes.moviemango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMangoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MovieMangoApplication.class, args);
+  }
+
+}
