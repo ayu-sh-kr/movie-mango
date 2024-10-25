b3e93598813ac09eefa3dfff0ed46846700afd1a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-26 00:36:05 +0530

    Create ProfileActionResponse.java
    Response for action related to profile such as create, get and update

---
+++ src/main/java/dev/archimedes/moviemango/profile/application/dto/response/ProfileActionResponse.java
@@ -0,0 +1,42 @@
+package dev.archimedes.moviemango.profile.application.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.archimedes.moviemango.profile.domain.Profile;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record ProfileActionResponse(
+    Long id,
+    String name,
+    Integer age,
+
+    @JsonFormat(
+        pattern = "yyyy-MM-dd"
+    )
+    LocalDate dob,
+    String street,
+    String state,
+    String zip,
+    String country,
+
+    @JsonFormat(
+        pattern = "yyyy-MM-dd hh:mm:ss"
+    )
+    LocalDateTime createdAt,
+
+    @JsonFormat(
+        pattern = "yyyy-MM-dd hh:mm:ss"
+    )
+    LocalDateTime updatedAt
+
+) {
+
+  public ProfileActionResponse(Profile profile) {
+    this(
+        profile.getId(), profile.getName(), profile.getAge(), profile.getDob(),
+        profile.getStreet().value(), profile.getState().value(), profile.getZip().value(),
+        profile.getCountry().value(), profile.getCreatedAt(), profile.getUpdatedAt()
+    );
+  }
+}
