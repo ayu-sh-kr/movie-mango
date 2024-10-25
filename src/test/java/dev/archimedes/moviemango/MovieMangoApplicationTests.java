ad5363c2180babb666dbcfdb9629227fbc9e768b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-25 22:22:27 +0530

    Create MovieMangoApplicationTests.java
    Default test provided by spring

---
+++ src/test/java/dev/archimedes/moviemango/MovieMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.archimedes.moviemango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMangoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
