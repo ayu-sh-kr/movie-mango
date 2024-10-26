0788e5bb0bbe13c73c75df16376b6b018e01a417
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-27 00:56:52 +0530

    Update MovieController.java
    make methods default and added new method to fetch movies

--- src/main/java/dev/archimedes/moviemango/movie/application/MovieController.java
+++ src/main/java/dev/archimedes/moviemango/movie/application/MovieController.java
@@ -2,6 +2,7 @@ package dev.archimedes.moviemango.movie.application;
 
 import dev.archimedes.moviemango.movie.application.dto.request.MovieCreateRequest;
 import dev.archimedes.moviemango.movie.application.dto.request.MovieDeleteRequest;
+import dev.archimedes.moviemango.movie.application.dto.request.MovieSearchRequest;
 import dev.archimedes.moviemango.movie.application.dto.request.MovieUpdateRequest;
 import dev.archimedes.moviemango.movie.application.dto.response.MovieActionResponse;
 import dev.archimedes.moviemango.movie.domain.Movie;
@@ -9,6 +10,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +21,12 @@ public class MovieController {
   private final MovieCreateUseCase movieCreateUseCase;
   private final MovieUpdateUseCase movieUpdateUseCase;
   private final MovieDeleteUseCase movieDeleteUseCase;
+  private final MovieSearchUseCase movieSearchUseCase;
 
   @PostMapping("/v1/movie")
   @ResponseStatus(HttpStatus.CREATED)
   @Operation(summary = "Use this API to create a Movie record")
-  public MovieActionResponse create(MovieCreateRequest request) {
+  MovieActionResponse create(@RequestBody MovieCreateRequest request) {
     Movie executed = movieCreateUseCase.execute(request);
     return new MovieActionResponse(executed);
   }
@@ -30,7 +34,7 @@ public class MovieController {
   @PatchMapping("/v1/movie")
   @ResponseStatus(HttpStatus.OK)
   @Operation(summary = "Use this API to update an existing Movie record")
-  public MovieActionResponse update(MovieUpdateRequest request) {
+  MovieActionResponse update(@RequestBody MovieUpdateRequest request) {
     Movie executed = movieUpdateUseCase.execute(request);
     return new MovieActionResponse(executed);
   }
@@ -38,8 +42,17 @@ public class MovieController {
   @DeleteMapping("/v1/movie")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @Operation(summary = "Use this API to delete an existing Movie record")
-  public void delete(MovieDeleteRequest request) {
+  void delete(@RequestBody MovieDeleteRequest request) {
     movieDeleteUseCase.execute(request);
+  }
+
+  @GetMapping("/v1/movie")
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Use this API to fetch movie records based on filter, sorts and limit.")
+  List<MovieActionResponse> fetch(@RequestBody MovieSearchRequest request) {
+    return movieSearchUseCase.execute(request)
+        .stream().map(MovieActionResponse::new)
+        .toList();
   }
 
 }
