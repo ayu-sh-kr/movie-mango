798a029ce57434b6a6340bf2dc97ef3c2a3ff216
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-26 10:38:37 +0530

    Update R__initial.sql
    modify database schema, added new Enum type gender and change drop to cascade drop

--- src/main/resources/db/migration/R__initial.sql
+++ src/main/resources/db/migration/R__initial.sql
@@ -1,18 +1,12 @@
-DROP TABLE IF EXISTS account;
-CREATE TABLE account (
-    id         SERIAL PRIMARY KEY,
-    email      VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP default now(),
-    updated_at TIMESTAMP
-);
+CREATE TYPE gender AS ENUM ('male', 'female', 'other');
 
-DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS profile CASCADE;
 CREATE TABLE profile (
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(255),
     age        INT,
     dob        DATE,
+    gender     gender,
     street     VARCHAR(255),
     state      VARCHAR(255),
     zip        VARCHAR(8),
@@ -20,5 +14,15 @@ CREATE TABLE profile (
     refer      BIGINT not null unique,
     created_at TIMESTAMP default now(),
     updated_at TIMESTAMP,
-    CONSTRAINT fk_account FOREIGN KEY (refer) REFERENCES account(id)
-)
+    CONSTRAINT fk_account FOREIGN KEY (refer) REFERENCES account (id)
+);
+
+
+DROP TABLE IF EXISTS account CASCADE;
+CREATE TABLE account (
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP default now(),
+    updated_at TIMESTAMP
+);
