fc19bdda763bbdf9b72a7bcf5c2571223713e37e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-26 18:14:28 +0530

    Update R__initial.sql
    added schema for movie table and created new enum type movie_genre

--- src/main/resources/db/migration/R__initial.sql
+++ src/main/resources/db/migration/R__initial.sql
@@ -1,7 +1,18 @@
 CREATE TYPE gender AS ENUM ('male', 'female', 'other');
 
+DROP TABLE IF EXISTS account CASCADE;
+CREATE TABLE account
+(
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP default now(),
+    updated_at TIMESTAMP
+);
+
 DROP TABLE IF EXISTS profile CASCADE;
-CREATE TABLE profile (
+CREATE TABLE profile
+(
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(255),
     age        INT,
@@ -17,12 +28,25 @@ CREATE TABLE profile (
     CONSTRAINT fk_account FOREIGN KEY (refer) REFERENCES account (id)
 );
 
+CREATE TYPE movie_genre AS ENUM(
+    'action', 'adventure', 'comedy', 'drama', 'fantasy',
+    'horror', 'mystery', 'romance', 'science fiction', 'thriller',
+    'western', 'animation', 'documentary', 'musical', 'crime', 'family',
+    'historical', 'war', 'sport', 'biography'
+);
 
-DROP TABLE IF EXISTS account CASCADE;
-CREATE TABLE account (
-    id         SERIAL PRIMARY KEY,
-    email      VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP default now(),
-    updated_at TIMESTAMP
+DROP TABLE IF EXISTS movie CASCADE;
+CREATE TABLE movie(
+    id          SERIAL PRIMARY KEY,
+    title       VARCHAR(255),
+    description VARCHAR(1000),
+    release     DATE,
+    duration    DOUBLE PRECISION,
+    genre       movie_genre,
+    director    VARCHAR(255),
+    rating      DOUBLE PRECISION,
+    language    VARCHAR(255),
+    origin      VARCHAR(255),
+    created_at  TIMESTAMP default now(),
+    updated_at  TIMESTAMP
 );
