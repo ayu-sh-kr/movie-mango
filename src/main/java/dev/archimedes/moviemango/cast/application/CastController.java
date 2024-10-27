5f3e3da1700a0e3647eba3940c1a51290f2beb59
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-27 22:36:34 +0530

    Endpoints for Cast Profile

---
+++ src/main/java/dev/archimedes/moviemango/cast/application/CastController.java
@@ -0,0 +1,44 @@
+package dev.archimedes.moviemango.cast.application;
+
+import dev.archimedes.moviemango.cast.application.dto.request.CastCreateRequest;
+import dev.archimedes.moviemango.cast.application.dto.request.CastDeleteRequest;
+import dev.archimedes.moviemango.cast.application.dto.request.CastUpdateRequest;
+import dev.archimedes.moviemango.cast.application.dto.response.CastActionResponse;
+import dev.archimedes.moviemango.cast.domain.Cast;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CastController {
+
+  private final CastCreateUseCase castCreateUseCase;
+  private final CastUpdateUseCase castUpdateUseCase;
+  private final CastDeleteUseCase castDeleteUseCase;
+
+  @PostMapping("/v1/cast")
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(summary = "Use this API to create a cast profile")
+  CastActionResponse create(CastCreateRequest request) {
+    Cast executed = castCreateUseCase.execute(request);
+    return new CastActionResponse(executed);
+  }
+
+  @PatchMapping("/v1/cast")
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Use this API to update a cast profile")
+  CastActionResponse update(CastUpdateRequest request) {
+    Cast executed = castUpdateUseCase.execute(request);
+    return new CastActionResponse(executed);
+  }
+
+  @DeleteMapping("/v1/cast")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @Operation(summary = "Use this API to delete a cast profile")
+  void delete(CastDeleteRequest request) {
+    castDeleteUseCase.execute(request);
+  }
+
+}
