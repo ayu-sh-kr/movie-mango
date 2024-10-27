dfc626d38613c638bf5d4eda9cd0b338bd094054
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-27 22:31:45 +0530

    Added validation.
    Validation for Id to ensure early exit before reaching controller

--- src/main/java/dev/archimedes/moviemango/movie/application/dto/request/MovieUpdateRequest.java
+++ src/main/java/dev/archimedes/moviemango/movie/application/dto/request/MovieUpdateRequest.java
@@ -2,10 +2,12 @@ package dev.archimedes.moviemango.movie.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 
 public record MovieUpdateRequest(
+
     @JsonProperty("movie_id")
     Long id,
     String title,
@@ -22,4 +24,12 @@ public record MovieUpdateRequest(
     String language,
     String origin
 ) {
+
+    public MovieUpdateRequest {
+        Assert.notNull(
+            id,
+            "Id is required to perform the update."
+        );
+    }
+
 }
