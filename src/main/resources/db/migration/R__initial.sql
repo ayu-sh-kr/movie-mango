568222b017012f79b74d42aa228aa5bd49995256
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-27 19:09:55 +0530

    Added new schema and type for movie_cast and cast

--- src/main/resources/db/migration/R__initial.sql
+++ src/main/resources/db/migration/R__initial.sql
@@ -1,4 +1,9 @@
-CREATE TYPE gender AS ENUM ('male', 'female', 'other');
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender') THEN
+    CREATE TYPE gender AS ENUM ('male', 'female', 'other');
+END IF;
+END $$;
 
 DROP TABLE IF EXISTS account CASCADE;
 CREATE TABLE account
@@ -28,25 +33,71 @@ CREATE TABLE profile
     CONSTRAINT fk_account FOREIGN KEY (refer) REFERENCES account (id)
 );
 
-CREATE TYPE movie_genre AS ENUM(
-    'action', 'adventure', 'comedy', 'drama', 'fantasy',
-    'horror', 'mystery', 'romance', 'science fiction', 'thriller',
-    'western', 'animation', 'documentary', 'musical', 'crime', 'family',
-    'historical', 'war', 'sport', 'biography'
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'movie_genre') THEN
+        CREATE TYPE movie_genre AS ENUM(
+            'action', 'adventure', 'comedy', 'drama', 'fantasy',
+            'horror', 'mystery', 'romance', 'science fiction', 'thriller',
+            'western', 'animation', 'documentary', 'musical', 'crime', 'family',
+            'historical', 'war', 'sport', 'biography'
+        );
+END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'movie') THEN
+    CREATE TABLE movie
+    (
+        id          SERIAL PRIMARY KEY,
+        title       VARCHAR(255),
+        description VARCHAR(1000),
+        release     DATE,
+        duration    DOUBLE PRECISION,
+        genre       movie_genre,
+        director    VARCHAR(255),
+        rating      DOUBLE PRECISION,
+        language    VARCHAR(255),
+        origin      VARCHAR(255),
+        created_at  TIMESTAMP default now(),
+        updated_at  TIMESTAMP
+    );
+END IF;
+END $$;
+
+DROP TABLE IF EXISTS cast_profile CASCADE;
+CREATE TABLE cast_profile
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255),
+    age        INT,
+    dob        DATE,
+    gender     gender,
+    country    VARCHAR(255),
+    created_at TIMESTAMP default now(),
+    updated_at TIMESTAMP
 );
 
-DROP TABLE IF EXISTS movie CASCADE;
-CREATE TABLE movie(
-    id          SERIAL PRIMARY KEY,
-    title       VARCHAR(255),
-    description VARCHAR(1000),
-    release     DATE,
-    duration    DOUBLE PRECISION,
-    genre       movie_genre,
-    director    VARCHAR(255),
-    rating      DOUBLE PRECISION,
-    language    VARCHAR(255),
-    origin      VARCHAR(255),
-    created_at  TIMESTAMP default now(),
-    updated_at  TIMESTAMP
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'movie_role') THEN
+        CREATE TYPE movie_role AS ENUM(
+            'lead actor', 'supporting actor', 'director', 'producer',
+            'cinematographer', 'editor', 'composer'
+        );
+END IF;
+END $$;
+
+DROP TABLE IF EXISTS movie_cast CASCADE;
+CREATE TABLE movie_cast
+(
+    id         SERIAL PRIMARY KEY,
+    cast_id    BIGINT not null,
+    movie_id   BIGINT not null,
+    role       movie_role not null,
+    created_at TIMESTAMP default now(),
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_movie FOREIGN KEY (movie_id) REFERENCES movie(id),
+    CONSTRAINT fk_cast FOREIGN KEY (cast_id) REFERENCES cast_profile(id)
+)
