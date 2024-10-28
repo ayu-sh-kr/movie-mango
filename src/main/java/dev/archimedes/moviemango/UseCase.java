c0169fae0efd3db40b83d56fd53c911593429452
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-28 21:45:21 +0530

    Docs and execution context change
    Make Annotation transactional by default

--- src/main/java/dev/archimedes/moviemango/UseCase.java
+++ src/main/java/dev/archimedes/moviemango/UseCase.java
@@ -1,13 +1,26 @@
 package dev.archimedes.moviemango;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/// The `UseCase` annotation is a custom stereotype annotation used to mark a class as a use case service in the application.
+///
+/// This annotation combines the functionality of the `@Service` and `@Transactional` annotations, indicating that the
+/// annotated class is a Spring service component and that its methods should be executed within a transactional context.
+///
+/// Usage of this annotation helps in clearly defining the use case layer in the application architecture, promoting
+/// separation of concerns and enhancing code readability and maintainability.
+///
+/// @see org.springframework.stereotype.Service
+/// @see org.springframework.transaction.annotation.Transactional
 @Service
+@Transactional
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UseCase {
