be194edc9a115df163d17288d4537962554e97ab
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-28 21:48:03 +0530

    Code optimization and added docs
    Replace check with Assert by spring instead of if

--- src/main/java/dev/archimedes/moviemango/user/application/AccountDeleteUseCase.java
+++ src/main/java/dev/archimedes/moviemango/user/application/AccountDeleteUseCase.java
@@ -6,7 +6,18 @@ import dev.archimedes.moviemango.user.domain.AccountDeleteEvent;
 import dev.archimedes.moviemango.user.domain.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.util.Assert;
 
+/// **AccountDeleteUseCase** class is a use case service to delete the account from the system.
+///
+///   The main responsibility is to validate if the account exist or not and then perform delete using
+///   [AccountRepository] and publish the delete event to delete [dev.archimedes.moviemango.profile.domain.Profile]
+///   associated with the [dev.archimedes.moviemango.user.domain.Account]
+///
+/// @see AccountDeleteUseCase#execute(AccountDeleteRequest)
+/// @see dev.archimedes.moviemango.user.domain.Account
+/// @see AccountDeleteRequest
+/// @see AccountDeleteEvent
 @UseCase
 @RequiredArgsConstructor
 public class AccountDeleteUseCase {
@@ -15,10 +26,19 @@ public class AccountDeleteUseCase {
   private final ApplicationEventPublisher eventPublisher;
 
 
+  /// Method to perform the delete action on Account upon delete request.
+  ///
+  /// Perform validation for existence of the account and throws error if fails.
+  ///
+  /// Throws [IllegalStateException]
+  /// @param request the request object containing the account ID to be deleted.
+  ///
   protected void execute(AccountDeleteRequest request) {
-    if (!accountRepository.existsById(request.id())) {
-      throw new RuntimeException("Account do not exists");
-    }
+
+    Assert.state(
+        accountRepository.existsById(request.id()),
+        "Account doesn't not exist"
+    );
 
     accountRepository.deleteById(request.id());
     eventPublisher.publishEvent(new AccountDeleteEvent(request.id()));
