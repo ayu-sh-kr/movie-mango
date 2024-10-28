6043557898aa046b464e8cab7a7d93377ba4af98
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-28 21:49:48 +0530

    Added java docs

--- src/main/java/dev/archimedes/moviemango/user/domain/AccountEmail.java
+++ src/main/java/dev/archimedes/moviemango/user/domain/AccountEmail.java
@@ -5,6 +5,14 @@ import dev.archimedes.moviemango.application.ApplicationUtils;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.util.Assert;
 
+/// **AccountEmail** is record to validate and manage state of the email field in the [Account].
+///
+///   Perform validation for null and blank email value.
+///  Throws [IllegalStateException] if the validation fails
+///
+/// @param value
+///
+/// @see dev.archimedes.moviemango.user.domain.Account
 public record AccountEmail(String value) {
   public AccountEmail {
     Assert.state(StringUtils.isNotBlank(value), "Email can't be null or blank");
--- src/main/java/dev/archimedes/moviemango/user/domain/AccountPassword.java
+++ src/main/java/dev/archimedes/moviemango/user/domain/AccountPassword.java
@@ -3,6 +3,17 @@ package dev.archimedes.moviemango.user.domain;
 import io.micrometer.common.util.StringUtils;
 import org.springframework.util.Assert;
 
+/// **Account Password** is record for handling the validation of the password field in account and hold the
+/// state of the password.
+///
+/// The main responsibility of this record is to validate the state of password field for null or blank value.
+/// Also check for the character length, whether it's valid or not.
+///
+/// Throws [IllegalStateException] if the validation fails.
+///
+/// @param value
+///
+/// @see dev.archimedes.moviemango.user.domain.Account
 public record AccountPassword(
     String value
 ) {
