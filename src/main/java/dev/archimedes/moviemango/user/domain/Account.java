f58edc199592584a8fc0f7646d66146c8f017a51
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-28 21:48:44 +0530

    Added Java docs

--- src/main/java/dev/archimedes/moviemango/user/domain/Account.java
+++ src/main/java/dev/archimedes/moviemango/user/domain/Account.java
@@ -4,6 +4,22 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+/// The **Account** class represents a user account entity in the system.
+///
+/// It includes fields for the account ID, email, password, and timestamps for creation and last update.
+/// The class uses Lombok annotations to reduce boilerplate code.
+///
+/// Attributes:
+///
+///     - id: Unique identifier for the account.
+///     - email: Email address associated with the account, encapsulated in an [AccountEmail] object.
+///     - password: Password for the account, encapsulated in an [AccountPassword] object.
+///     - createdAt: Timestamp when the account was created.
+///     - updatedAt: Timestamp when the account was last updated.
+///
+///
+/// @see AccountEmail
+/// @see AccountPassword
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
