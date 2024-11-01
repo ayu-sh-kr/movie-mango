4711ad8533e295862cb7c345410a7efcf8623dda
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-01 23:59:19 +0530

    Added endpoints to fetch cast associated with certain movie by its name or id

--- src/main/java/dev/archimedes/moviemango/moviecast/application/MovieCastController.java
+++ src/main/java/dev/archimedes/moviemango/moviecast/application/MovieCastController.java
@@ -1,13 +1,17 @@
 package dev.archimedes.moviemango.moviecast.application;
 
 import dev.archimedes.moviemango.moviecast.application.dto.request.MovieCastCreateRequest;
+import dev.archimedes.moviemango.moviecast.application.dto.request.MovieCastFetchRequest;
 import dev.archimedes.moviemango.moviecast.application.dto.request.MovieCastUpdateRequest;
 import dev.archimedes.moviemango.moviecast.application.dto.response.MovieCastActionResponse;
+import dev.archimedes.moviemango.moviecast.application.dto.response.MovieCastJointResponse;
 import dev.archimedes.moviemango.moviecast.domain.MovieCast;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ public class MovieCastController {
 
   private final MovieCastCreateUseCase movieCastCreateUseCase;
   private final MoveCastUpdateUseCase moveCastUpdateUseCase;
+  private final MovieCastFetchUseCase movieCastFetchUseCase;
 
   @PostMapping("/v1/movie-cast")
   @ResponseStatus(HttpStatus.CREATED)
@@ -32,6 +37,13 @@ public class MovieCastController {
   MovieCastActionResponse update(@RequestBody MovieCastUpdateRequest request) {
     MovieCast executed = moveCastUpdateUseCase.execute(request);
     return new MovieCastActionResponse(executed);
+  }
+
+  @GetMapping("/v1/movie-cast")
+  @ResponseStatus(HttpStatus.OK)
+  @Operation(summary = "Use this API to fetch the casts in the movie by name or ID")
+  List<MovieCastJointResponse> get(@RequestBody MovieCastFetchRequest request) {
+    return movieCastFetchUseCase.execute(request);
   }
 
 }
