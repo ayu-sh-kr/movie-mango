c2330f636cd557da2925c0c74153699c21ae8439
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-03 23:26:24 +0530

    Added docs for the service

--- src/main/java/dev/archimedes/moviemango/moviecast/application/MovieCastFetchUseCase.java
+++ src/main/java/dev/archimedes/moviemango/moviecast/application/MovieCastFetchUseCase.java
@@ -8,6 +8,15 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+/// `MovieCastFetchUseCase` is a service use-case to fetch cast data by id or the cast name.
+///
+/// For match with name it prefers `name like :value` and based on that choose all the matching records and
+/// fetch it.
+///
+/// For match with id, it will fetch the unique record associated with the id.
+///
+/// @see MovieCastRepository
+/// @see dev.archimedes.moviemango.moviecast.domain.MovieCast
 @UseCase
 @RequiredArgsConstructor
 public class MovieCastFetchUseCase {
